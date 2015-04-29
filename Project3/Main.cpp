e23c312acd95addd01bd920bb70959ec4359bc39
ericlytle <elmp7@mail.umkc.edu>
2015-04-28 19:03:20 -0500

    Main

--- Project3/Main.cpp
+++ Project3/Main.cpp
@@ -9,11 +9,8 @@ using namespace std;
 
 int main(){ 
 	Morse<string> MT("morse.txt");; //I feel that morse tree should be a single object contining
-	cout << MT.encode("Eric brad luke");
-
-
-	//cout << endl << MT.decode(encoder.getEncodedMessage());
-
+	cout << MT.decode("__. _.") << endl; //decode test
+	cout << MT.encode("eric Brad LuKe") << endl; //encode test with random case | is delimiter between words
 
 	system("pause");
 
